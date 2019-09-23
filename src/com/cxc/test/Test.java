b41f2ec73c43eefe411bf80409051505c68e9190
changxianchuang <15638553436@163.com>
2019-09-23 13:58:55 +0800

    cxc-dev3 tiaojiao

--- src/com/cxc/test/Test.java
+++ src/com/cxc/test/Test.java
@@ -3,7 +3,7 @@ package com.cxc.test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("ccc");
+        System.out.println("cccd");
         System.out.println("bbbbbbbbb");
         System.out.println("ccccccccc");
     }
