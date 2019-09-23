c61ba03c1bcdd1d5e9dfd2271243f6a1e4ef128b
changxianchuang <15638553436@163.com>
2019-09-23 13:15:23 +0800

    master 修改了1

--- src/com/cxc/test/Test.java
+++ src/com/cxc/test/Test.java
@@ -3,7 +3,7 @@ package com.cxc.test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("aaaaaaaaaa");
+        System.out.println("aaaa");
         System.out.println("bbbbbbbbb");
         System.out.println("ccccccccc");
     }
