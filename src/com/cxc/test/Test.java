d0b062f0c86a591c1ca3243a0be35e620326b0ff
changxianchuang <15638553436@163.com>
2019-09-23 14:00:47 +0800

    youyici

--- src/com/cxc/test/Test.java
+++ src/com/cxc/test/Test.java
@@ -3,7 +3,7 @@ package com.cxc.test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("cccd");
+        System.out.println("cccdd");
         System.out.println("bbbbbbbbb");
         System.out.println("ccccccccc");
     }
