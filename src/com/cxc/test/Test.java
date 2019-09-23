caa2eb3458a958571ba56f46adf019f9324f43ca
changxianchuang <15638553436@163.com>
2019-09-23 12:57:33 +0800

    cxc-dev 提交

--- src/com/cxc/test/Test.java
+++ src/com/cxc/test/Test.java
@@ -2,6 +2,8 @@ package com.cxc.test;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("aaaaaaaaaa");
+        System.out.println("bbbbbbbbb");
     }
 }
