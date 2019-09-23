c6a8e40b4ccfbd47b1d75a004020789f7de77538
changxianchuang <15638553436@163.com>
2019-09-23 13:21:13 +0800

    cxc-dev 第二次提交

--- src/com/cxc/test/Test.java
+++ src/com/cxc/test/Test.java
@@ -3,7 +3,7 @@ package com.cxc.test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("aaaa");
+        System.out.println("aaaa啊啊啊");
         System.out.println("bbbbbbbbb");
         System.out.println("ccccccccc");
     }
