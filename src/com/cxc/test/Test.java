ecc904db050c073a1c9b11e0bf45e870a11260a2
changxianchuang <15638553436@163.com>
2019-09-23 13:33:39 +0800

    cxc-dev ddd修改

--- src/com/cxc/test/Test.java
+++ src/com/cxc/test/Test.java
@@ -3,7 +3,7 @@ package com.cxc.test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("aaaa啊啊啊");
+        System.out.println("dddd");
         System.out.println("bbbbbbbbb");
         System.out.println("ccccccccc");
     }
